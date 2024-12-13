4f093aaa379c802b617364c019ca47b61beff4bd
PauloVisbeck <pls_ray@yahoo.com.br>
2024-12-13 12:43:06 -0300

    Quinto commit. Trabalhado na responsividade da página e quando a mesma atinge largura inferior a 1200px o menu superior é transferido para o botão estilo hamburguer.

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const items = containder.querySelectorAll(".list .item")
 const indicator = document.querySelector(".indicators")
 const traits = indicator.querySelectorAll("ul li")
 const list = containder.querySelector(".list ")
+const menuToggle = document.getElementById('menu-toggle');
+const nav = document.querySelector('nav');
 
 let active = 0
 let firstPosition = 0
@@ -38,7 +40,7 @@ function updateActiveCar(direction) {
 
 prevButton.addEventListener("click", () => {
     
-    const newDirection = -1          
+    const newDirection = 1          
     
     active = active - 1 < firstPosition ? lastPosition : active - 1
     updateActiveCar(newDirection);   
@@ -47,8 +49,13 @@ prevButton.addEventListener("click", () => {
 
 nextButton.addEventListener("click", () => {
     
-    const newDirection = 1    
+    const newDirection = -1    
  
     active = active + 1 > lastPosition ? firstPosition : active + 1
     updateActiveCar(newDirection);
 })
+
+/* O método classList.toggle verifica se a classe especificada (show, no caso) já está presente no elemento. Se a classe não está presente ela será adicionada. Se a classe já está presente ela será removida. (O mesmo botão adiciona e remove a classe com esse método)  */
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('show');
+});
